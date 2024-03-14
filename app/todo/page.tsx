25c85bed3c316c88f982fe6e599392f42215ed6e
AliMoharramy <alimoharrami.dev@gmail.com>
2024-03-14 15:03:33 +0330

    pomotimer full page and delete main and move to page

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,6 +1,46 @@
+"use client";
 import Navlinks from "../ui/todo/navb-links";
-import MainTodo from "../ui/todo/main";
-export default function todoPage() {
+import DoneTasks from "../ui/todo/done-tasks";
+import InfoBox from "../ui/todo/info-box";
+import TodoList from "../ui/todo/todo-list";
+import WorkingTask from "../ui/todo/workin-task";
+import data from "..//lib/data.json";
+import PomoTimer from "../ui/todo/pomoTimer";
+import { useState } from "react";
+export default function TodoPage() {
+  const [todo, setTodo] = useState(data.cards);
+  const [workingOn, setWorkingOn] = useState<number>(NaN);
+  const [doneTasks, setDoneTasks] = useState<number[]>([]);
+  const [taskInfo, setTaskInfo] = useState<number>(NaN);
+  const [fullPomp, setFullPomo] = useState<boolean>(false);
+
+  function compliteTask() {
+    // move task from working on to doneTask and clear working
+    workingOn && setDoneTasks([...doneTasks, workingOn]);
+    setWorkingOn(NaN);
+  }
+  function handleOnDrag(e: React.DragEvent, id: number) {
+    const target = e.target as Element;
+    e.dataTransfer.setData("checkId", String(id));
+    target.classList.add("ondragItem");
+  }
+  function handleDropDown(e: React.DragEvent) {
+    const target = e.target as Element;
+    target.classList.remove("ondragItem");
+  }
+  function handleOnDrop(e: React.DragEvent) {
+    const checkId = e.dataTransfer.getData("checkId") as string;
+    setWorkingOn(Number(checkId));
+    // clear working task from todo list
+    setTodo(todo.filter((item) => item.id !== Number(checkId)));
+  }
+  function handleDragOver(e: React.DragEvent) {
+    e.preventDefault();
+  }
+  //
+  function showTaskInfo(e: number) {
+    setTaskInfo(e);
+  }
   return (
     <div className=" text-white bg-black min-h-screen">
       <Navlinks
@@ -10,7 +50,35 @@ export default function todoPage() {
         ]}
         active={0}
       />
-      <MainTodo />
+      <div className="grid grid-cols-5 grid-rows-5 gap-px pt-12  w-11/12 mx-auto">
+        <TodoList
+          onDragStart={handleOnDrag}
+          handleDropDown={handleDropDown}
+          showTaskInfo={showTaskInfo}
+          //send wokingOn to clear that task from todo list
+          todo={todo}
+        />
+        <DoneTasks doneTasks={doneTasks} showTaskInfo={showTaskInfo} />
+        <WorkingTask
+          workingOn={workingOn}
+          onDrop={handleOnDrop}
+          onDragOver={handleDragOver}
+          compliteTask={compliteTask}
+        />
+        {workingOn ? (
+          <PomoTimer />
+        ) : (
+          <>
+            <InfoBox taskInfo={taskInfo} showTaskInfo={showTaskInfo} />
+            <div className="bg-bodyBox rounded-s-2xl p-2 col-start-3 col-span-2 row-span-2">
+              middle part
+            </div>
+            <div className="bg-bodyBox rounded-e-2xl rounded-t-2xl p-2 row-span-5 col-start-5 row-start-1">
+              right part
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
--- app/ui/todo/pomoTimer.tsx
+++ app/ui/todo/pomoTimer.tsx
@@ -1,18 +1,38 @@
 "use client";
 import { useStopwatch } from "react-timer-hook";
 import { archivoBlack } from "../fonts";
+import { useState } from "react";
+import clsx from "clsx";
 export default function PomoTimer() {
   const { totalSeconds, seconds, minutes, hours, start, pause, reset } =
     useStopwatch({ autoStart: true });
+  const [fullPage, setFullPage] = useState<boolean>(false);
   return (
-    <div className="bg-bodyBox p-5 rounded-2xl row-span-5 col-span-3 relative">
+    <div
+      className={clsx("bg-bodyBox p-5 rounded-2xl row-span-5 col-span-3", {
+        relative: !fullPage,
+      })}
+    >
       <div
-        className={`${archivoBlack.className} pomotimer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-8xl py-28 w-1/2 text-center `}
+        className={clsx(
+          archivoBlack.className,
+          "pomotimer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-8xl py-28 text-center",
+          {
+            "w-1/2": !fullPage,
+            "w-full h-full flex justify-center items-center text-9xl": fullPage,
+          }
+        )}
       >
         {minutes + hours * 60 < 10 ? 0 : ""}
         {minutes + hours * 60}:{seconds < 10 ? 0 : ""}
         {seconds}
       </div>
+      <button
+        className={clsx({ absolute: fullPage, "left-0 top-0 m-6": fullPage })}
+        onClick={(e) => setFullPage(fullPage ? false : true)}
+      >
+        full page
+      </button>
     </div>
   );
 }
